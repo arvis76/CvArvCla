74f67f7c5b357993885d18012d93bb2878742c12
arvis76 <arvisclas@gmail.com>
2025-07-16 16:15:30 +0100

    Add project files.

---
+++ CvArvCla/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+
+app.UseHsts();
+
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
