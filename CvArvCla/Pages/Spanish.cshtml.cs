931add7a7c92ec6074787eaae64ec28864b8cd5c
arvis76 <arvisclas@gmail.com>
2025-07-18 12:38:34 +0100

    added file for swedish and spanish

---
+++ CvArvCla/Pages/Spanish.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CvArvCla.Pages
+{
+    public class SpanishModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
